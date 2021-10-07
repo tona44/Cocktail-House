8c82e408a9173dc0752308436012acb87129ff5e
tona44 <kuryu.waku@gmail.com>
2021-10-07 18:58:27 +0000

    [Add] devise,ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  root to: 'homes#top'
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      # users直下の[:id(PK)]が付与される
+      get :followings
+      get :followers
+    end
+    resource :relationships, only: [:create, :destroy]
+
+  end
+
+  get '/users/:user_id/notifications', to: 'notification#index'
+  get '/users/:user_id/favorites', to: 'favorites#index'
+
+  resources :cocktails do
+    resource :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+
+    collection do
+      get :search
+    end
+
+  end
+
+  resource :users
+
 end
