bdff077f4ad442675a81d269a8b03bf42cc6de66
tona44 <kuryu.waku@gmail.com>
2021-10-12 05:09:11 +0000

    [Add] カクテル投稿フォーム

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,15 +1,26 @@
 class CocktailsController < ApplicationController
 
   def index
+    
   end
 
   def new
+    @cocktail = Cocktail.new
+  end
+  
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.create
+    redirect_to cocktail_path
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def update
   end
 
   def search
