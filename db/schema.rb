b145d3355a9eed925473d32e29c552745de36fb9
tona44 <kuryu.waku@gmail.com>
2021-10-12 12:21:12 +0000

    [Add] materialテーブル NULL制約解除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_105759) do
+ActiveRecord::Schema.define(version: 2021_10_12_121801) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -90,8 +90,8 @@ ActiveRecord::Schema.define(version: 2021_10_12_105759) do
 
   create_table "materials", force: :cascade do |t|
     t.integer "cocktail_id", null: false
-    t.string "name", null: false
-    t.string "quantity", null: false
+    t.string "name"
+    t.string "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
