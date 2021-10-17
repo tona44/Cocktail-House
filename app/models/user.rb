eac42faf40c4b75bed9ebeb65c4f348f80a056cb
tona44 <kuryu.waku@gmail.com>
2021-10-17 08:08:50 +0000

    [Add] フォローフォロワー機能（非同期通信）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+
+
   has_one_attached :profile_image
 
   has_many :cocktails, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_many :relationships, foreign_key: :following_id, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :following
+
+    def is_followed_by?(user)
+      reverse_of_relationships.find_by(following_id: user.id).present?
+    end
+
 
 end
