553a9e2b901c2b3f04b02be2e634c2ad1ff4cbe8
tona44 <kuryu.waku@gmail.com>
2021-10-27 17:26:41 +0000

    [Debug] バグ修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.notifications.all.page(params[:page]).per(5)
+    @notifications = current_user.notifications.all.page(params[:page]).per(10)
 
     @notifications.each do |notification|
       notification.update_attribute(:checked, true)
